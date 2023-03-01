9b6e4e09296dfb0a563907522706fdba7d230145
Debsvalerio <103152279+Debsvalerio@users.noreply.github.com>
2023-03-01 08:30:30 -0400

    assert

--- __tests__/web/wd/comprarPassagemWD.spec.js
+++ __tests__/web/wd/comprarPassagemWD.spec.js
@@ -7,14 +7,14 @@ const assert = require('assert')
 describe("Comprar Passagem WD", () => {
     let driver
 // Inicialização
-    beforeEach("", async () => {
+    beforeEach(async () => {
         driver = await new Builder().forBrowser('chrome').build()
         driver.manage().setTimeouts({implicit: 60000})
         driver.manage().window().maximize()
     })
 
 // Finalização
-    afterEach("", async () => {
+    afterEach(async () => {
         await driver.quit()
     })
 
@@ -22,12 +22,19 @@ describe("Comprar Passagem WD", () => {
     it("Comprar Passagem SP - Dublin", async () => {
         await driver.get('https://blazedemo.com')
         {
-            const dropdown = await driver.findElement(By.name("fromPort"))
-            await dropdown.findElement(By.xpath("//option[. = 'São Paolo']")).click
+            const dropdown = await driver.findElement(By.name('fromPort'))
+            await dropdown.findElement(By.xpath("//option[. = 'São Paolo']")).click()
         }
         {
-            const dropdown = await driver.findElement(By.name("toPort"))
-            await dropdown.findElement(By.xpath("//option[. = 'Dublin']")).click
+            const dropdown = await driver.findElement(By.name('toPort'))
+            await dropdown.findElement(By.xpath("//option[. = 'Dublin']")).click()
         }
+        // Clicar no botão Find Flights (Procurar Voos)
+        await driver.findElement(By.css('input.btn.btn-primary')).click()
+
+        // Validar o título da guia e a frase de titulo da seleção de voos
+        
+        assert(await driver.getTitle() == 'BlazeDemo - reserve')
+        assert(await driver.findElement(By.xpath('//h3')).getText() == 'Flights from São Paolo to Dublin:')
     })
 })
