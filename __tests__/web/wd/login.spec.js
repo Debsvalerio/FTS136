92252fec8c9d95cfeada205d79b9c5a31317ad0a
Debsvalerio <103152279+Debsvalerio@users.noreply.github.com>
2023-03-10 23:48:06 -0400

    Ajuste final login.spec

--- __tests__/web/wd/login.spec.js
+++ __tests__/web/wd/login.spec.js
@@ -1,6 +1,13 @@
-const { Builder, By } = require('selenium-webdriver')
+const { Builder, By, Key } = require('selenium-webdriver')
 require('chromedriver')
 const assert = require('assert')
+const fsp = require('fs').promises
+
+// Funções de apoio
+async function takeScreenshot(driver, file) {
+    let image = await driver.takeScreenshot()
+    await fsp.writeFile(file, image, 'base64')
+}
 
 describe('Login', function () {
     let driver
@@ -19,20 +26,28 @@ describe('Login', function () {
 
     it('Testando Login', async function() {
         await driver.get('https://www.blazedemo.com')
-        await driver.findElement(By.LinkText('home')).click()
+
+        takeScreenshot(driver, 'print1 - home.png')
+
+        await driver.findElement(By.linkText('home')).click()
         
         //Validar pra ver se entrou realmente na pagina de login
         assert(await driver.findElement(By.css('div.panel-heading')).getText() == 'Login')
+
         // Escreve no campo 'email'
         await driver.findElement(By.id('email')).sendKeys('test@teste.com.br')
+
         // escreve no campo 'senha'
-        await driver.findElement(By.id('password').sendKeys('12345678'))
+        await driver.findElement(By.id('password')).sendKeys('12345678')
+
         // clica no botão 'login'
         await driver.findElement(By.css('button.btn.btn-primary')).click()
+
         // valida o texto da guia
-        assert(await driver.getText() == 'Page Expired')
+        assert(await driver.getTitle() == 'Page Expired')
+
         // valida o texto da pagina
-        assert(await driver.findElement(By.css('div.flex-center.position-ref.full-height')).getText() == '419 Page Expired')
+        assert(await driver.findElement(By.css('div.flex-center.position-ref.full-height')).getText() == '419\nPage Expired')
        
     })
 })
